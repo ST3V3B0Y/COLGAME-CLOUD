5336fd11c97691f6d8d01e26f7f8e1bd39f1c5d2
steve <st3v3njaramillo@gmail.com>
2025-10-27 14:23:54 -0500

    Inicio

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div>
+        <nav>
+    
+        </nav>
+        <header>
+          <h1>Bienvenido a ColGame Cloud</h1>
+          <button>Empezar tour</button> 
+        </header>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home;
